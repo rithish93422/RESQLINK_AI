2150415c690bbdd4a8029e5c2de1caddd665b214
Rithish <rithish93422@gmail.com>
2026-08-09 10:15:27 +0530

    Fix mobile routing with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 import "./styles/global.css";
@@ -7,6 +8,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
